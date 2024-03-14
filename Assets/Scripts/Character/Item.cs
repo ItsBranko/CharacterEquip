a6d3756728d7989860e99f6a72eb45111500f942
ItsBranko <branko.zikic@student.forsbergsskola.se>
2024-03-14 18:06:18 +0100

    Meta files and going through the code to understand it

--- Assets/Scripts/Character/Item.cs
+++ Assets/Scripts/Character/Item.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public int level;
-    public float intelligenceMultiplier; // 0.1
+    public float intelligenceMultiplier; // 1.1
 
     public int maxLevel;
     public int maxStrength;
@@ -21,11 +21,11 @@ public class Item : MonoBehaviour
         if (IsEquipped) return;
         
         // It is supposed to give a x% bonus on our intelligence
-        // e.g. 22 => 33 (Level 5 * 0.1 per Level = 50% Bonus)
-        Stats.SetIntelligence((int)(Stats.GetIntelligence() * level * intelligenceMultiplier));
+        // e.g. 23 => 33 (Level 5 * 0.1 per Level = 50% Bonus)
+        Stats.SetIntelligence((int)(Stats.GetIntelligence() + (level * intelligenceMultiplier * Stats.GetIntelligence())));     //a lot of ()()()() maybe i can fix that a bit later
         
-        // + 15 Strength, because Level 5/10 and MaxStrength is 30
-        //                  = 50% of 30 = 15
+        // + 16 Strength, because Level 5/10 and MaxStrength is 30
+        //                  = 51% of 30 = 15
         Stats.Strength += (int)((float)level / maxLevel * maxStrength);
         _equipped = true;
     }
@@ -34,7 +34,7 @@ public class Item : MonoBehaviour
     {
         if (!IsEquipped) return;
         
-        Stats.SetIntelligence((int)(Stats.GetIntelligence() / (level * intelligenceMultiplier)));
+        Stats.SetIntelligence((int)(Stats.GetIntelligence() - (level * intelligenceMultiplier * Stats.GetIntelligence())));
         // formula is correct
         Stats.Strength -= (int)((float)level / maxLevel * maxStrength);
         _equipped = false;
